979073ae456f8d05e26966e0e72956b7c367f382
Gintaras <99560038+Gintaras68@users.noreply.github.com>
2026-04-24 12:56:09 +0300

    Component "Header"

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import logoImg from '../../img/logo.png';
+import bellImg from '../../img/bell-fill-svgrepo-com.svg';
 import style from './Header.module.css';
 
 function Header() {
   console.log(style);
-  
+
   return (
-    <header className={style.header}>Header</header>
-  )
+    <header className={style.header}>
+      <div className={style.leftSide}>
+        <img src={logoImg} alt="Logo" />
+        <form className={style.searchForm}>
+          <input type="text" placeholder='Search everything'/>
+        </form>
+      </div>
+
+      <nav className={style.mainNavigate}>
+        <a href='/' className={style.activeLink}>Projects</a>
+        <a href='/' >Settings</a>
+        <a href='/' >Help</a>
+      </nav>
+
+      <div className={style.rightSide}>
+        <button className={style.notes}>
+          <img src={bellImg} alt='Your notes'/>
+        </button>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
